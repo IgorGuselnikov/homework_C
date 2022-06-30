b0a755f59a1672e5a1ac62e3c6ee4066706753c0
Igor <guselnikov.uniikm@yandex.ru>
2022-06-30 12:10:38 +0500

    second task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Введите первое число:");
+/*Console.WriteLine("Введите первое число:");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
@@ -23,4 +23,32 @@ else if (firstNumber < secondNumber)
 else
 {
     Console.WriteLine("Введенные числа равны, необходимо обязательно вводить разные числа!");    
+}*/
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (firstNumber > secondNumber && firstNumber > thirdNumber)
+{
+    max = firstNumber;
+    Console.WriteLine("Максимальное число из трех:" + firstNumber);
 }
+else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+{
+    max = secondNumber;
+    Console.WriteLine("Максимальное число из трех:" + secondNumber);
+}
+else
+{
+    max = thirdNumber;
+    Console.WriteLine("Максимальное число из трех:" + thirdNumber);    
+}
+
+
+
