70141290353e6f7ba9b141b4c8c0b7b208ff6c13
Igor <guselnikov.uniikm@yandex.ru>
2022-06-30 12:12:54 +0500

    fourth task

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ else
 }*/
 
 //Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
-Console.WriteLine("Введите число:");
+/*Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
 
@@ -62,6 +62,32 @@ if (number%2 == 0)
 else 
 {
     Console.WriteLine("Число " + number + " не является четным");
+}*/
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 1)
+{
+    int i = 1;
+    while (i <= N)
+    {
+        if (i%2 != 0)
+        {
+            i++;
+        }
+        else
+        {
+            Console.Write(i + " ");
+            i++;
+        }
+    }
+
+}
+else
+{
+    Console.WriteLine("Введенное число не удовлетворяет условиям");
 }
 
 
